c5a4c04ed25fba1c8302710037beeb1b45255857
maryam <maryam@PGs-MacBook-Pro-2.local>
2016-04-13 17:49:44 +0430

    Dockerize

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 var logger = require('./logger')
 var express = require('express');
 var app = express();
+var redis = require("redis");
+var client = redis.createClient({host: 'redis', port: 6379});
 
-app.get('/api/users',
-logger,
+// if you'd like to select database 3, instead of 0 (default), call
+// client.select(3, function() { /* ... */ });
+
+client.on("error", function (err) {
+  console.log("Error " + err);
+});
+client.set('1001', 'yas')
+client.set('1002', 'maryam')
+
+app.get('/api/users', logger,
 function access(req, res, next) {
   console.log("Middleware");
   if (req.query.id != 1) {
-    res.send("You don't have access!")
+    client.get(req.query.id, (err, reply) => {
+        res.send(reply + " don't have access!")
+    })
+
   } else {
     next()
     console.log("main logger");
@@ -26,4 +39,6 @@ function(req, res) {
     }]
   );
 });
-app.listen(3000);
+app.listen(8080, function(){
+  console.log('listening on port 8080')
+})
